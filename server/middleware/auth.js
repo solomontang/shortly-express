0f8ba2845f6dc99cb16dcde86533301196177d68
solomontang <solomoncztang@gmail.com>
2017-06-15 12:22:05 -0700

    Refactor krohani auth.js createSession

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,70 +1,49 @@
 const models = require('../models');
 const Promise = require('bluebird');
 
+
+
 module.exports.createSession = (req, res, next) => {
+
+  var setSession = (session) => {
+    console.log('new session', session);
+    res.cookies = {'shortlyid': {'value': session.hash}};
+    req.session = session;
+    next();
+  };
   
-  console.log('create session ', req.cookies);
-  //check for user-agent
-  //if mismatch
-    //delete record
-  //else 
-    //do rest of this
-  if (!Object.keys(req.cookies).length ) {
+  //if no cookie is on req
+    //make session and attach to req and appropriate cookie to res
+  // else
+    //check if cookie on req matches DB hash
+      //if true, attach session to req
+      //else make new session and attach to req, and cookie to res.
+  
+  if (Object.keys(req.cookies).length === 0) {  //NO COOKIE
+    console.log('No cookies found, creating session');
     models.Sessions.create(Date.now())
     .then( (status) => {
       return models.Sessions.get({'id': status.insertId})
-      .then( (session) => {
-        // req.param('session', {'hash': session.hash})
-        req.session = {'hash': session.hash};
-        res.cookies = {'shortlyid': {'value': session.hash}};
-        next();
-      });
+      .then(setSession);
     })
     .catch( (err) => {
       console.log('Error creating session ', err);
     });
-  } else {
-    //we havea  cookie but check it if it matches sessions on the db
-      //if doesn't match 
-    req.session = {'hash': req.cookies.shortlyid};
-    next();
-  }
-};
-
-/************************************************************/
-// Add additional authentication middleware functions below 
-/************************************************************/
-
-//Make a session without user,
-//when the user login/signup, update the session table
-//so that the current session is associated with the userId?
-
-/*
-  if (Object.keys(req.cookies).length) {
-   
-  } else {  //no cookies then create session
-    if (!req.body.username) {
-  
-    } else {
-    
-    }
-   
-    models.Users.get({username: username})
-    .then ( (user) => {
-
-      console.log('retrieved user ', user);
-      return models.Session.create(user.id)
-
-      .then ( (session) => {
-        console.log('created session ', session);
-        req.session = {'hash': session.hash};
-      });
+  } else { //WE HAVE A COOKIE
+    console.log('req.cookies', req.cookies, '\nreq.session', req.session, '\nres.cookies', res.cookies);
+    models.Sessions.get({'hash': req.cookies.shortlyid})
+    .then( (retrievedSession) => {
+      if (retrievedSession) {
+        req.session = retrievedSession;
+        next();
+      } else {
+        models.Sessions.create(Date.now())
+        .then(setSession);
+      }
     })
     .catch( (err) => {
-      console.log('Error creating either a session or getting a user', req.body.username, err);
+      console.log('Error retrieving session', err);
     });
-    
+    //req.session = {'hash': req.cookies.shortlyid};
   }
-  next();
-
-*/
+};
